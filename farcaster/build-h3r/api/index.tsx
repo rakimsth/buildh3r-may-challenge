f62bd26512e869d8798fe9a0bfd69e6b1f9bdf3c
Raktim Shrestha <rakimsth@gmail.com>
2024-05-08 14:58:47 +0545

    learned about farcaster frames

---
+++ farcaster/build-h3r/api/index.tsx
@@ -0,0 +1,65 @@
+import { Button, Frog } from "frog";
+import { devtools } from "frog/dev";
+import { serveStatic } from "frog/serve-static";
+// import { neynar } from 'frog/hubs'
+import { handle } from "frog/vercel";
+
+// Uncomment to use Edge Runtime.
+// export const config = {
+//   runtime: 'edge',
+// }
+
+export const app = new Frog({
+  assetsPath: "/",
+  basePath: "/api",
+  // Supply a Hub to enable frame verification.
+  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
+});
+
+app.frame("/", (c) => {
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: "center",
+          background: "linear-gradient(to right, #432889, #17101F)",
+          backgroundSize: "100% 100%",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+          height: "100%",
+          justifyContent: "center",
+          textAlign: "center",
+          width: "100%",
+        }}
+      >
+        <div
+          style={{
+            color: "white",
+            fontSize: 60,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          "Hi, I am Raktim. Check out my Bio!"
+        </div>
+      </div>
+    ),
+    intents: [
+      <Button.Link href="https://raktim.com.np">Website</Button.Link>,
+      <Button.Link href="https://github.com/rakimsth">Github</Button.Link>,
+    ],
+  });
+});
+
+// @ts-ignore
+const isEdgeFunction = typeof EdgeFunction !== "undefined";
+const isProduction = isEdgeFunction || import.meta.env?.MODE !== "development";
+devtools(app, isProduction ? { assetsPath: "/.frog" } : { serveStatic });
+
+export const GET = handle(app);
+export const POST = handle(app);
